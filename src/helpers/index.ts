d7b61b7da4ad513ed3fae0c744fa5008365a53c3
mondaychen <monday.chen@gmail.com>
2024-03-21 17:57:07 -0400

    fix: new lib exporting

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,7 @@
+import { type ToolOperation } from "./vision-agent/tools";
+export { ToolOperation };
 export { DomActions } from "./rpc/domActions";
 export { callRPC, callRPCWithTab } from "./rpc/pageRPC";
 export { attachDebugger, detachDebugger } from "./chromeDebugger";
-import performAction from "./rpc/performAction";
-export { performAction };
-export type { Action } from "./rpc/performAction";
+import { operateTool } from "./rpc/performAction";
+export { operateTool };
