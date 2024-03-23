5b786f54e11cd1ddd09bdbf76e7abee5712556b1
mondaychen <monday.chen@gmail.com>
2024-03-23 17:17:10 -0400

    feat: add gpt-4-turbo-preview

--- src/common/ModelDropdown.tsx
+++ src/common/ModelDropdown.tsx
@@ -21,7 +21,8 @@ const ModelDropdown = () => {
     >
       <option value="gpt-3.5-turbo-16k">GPT-3.5 Turbo (16k)</option>
       <option value="gpt-4">GPT-4</option>
-      <option value="gpt-4-vision-preview">GPT-4 Vision Preview</option>
+      <option value="gpt-4-turbo-preview">GPT-4 Turbo (Preview)</option>
+      <option value="gpt-4-vision-preview">GPT-4 Vision (Preview)</option>
     </Select>
   );
 };
