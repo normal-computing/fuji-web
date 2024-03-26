060a19780e8feca5390fd43d126d12c00d0c428f
mondaychen <monday.chen@gmail.com>
2024-03-26 16:39:53 -0400

    feat: add links in footer to collect feedbacks

--- src/common/App.tsx
+++ src/common/App.tsx
@@ -1,11 +1,14 @@
 import {
+  Link,
   Box,
   ChakraProvider,
   Heading,
   HStack,
   IconButton,
+  Icon,
 } from "@chakra-ui/react";
 import { SettingsIcon } from "@chakra-ui/icons";
+import { FaDiscord } from "react-icons/fa6";
 import React, { useState } from "react";
 import { useAppState } from "../state/store";
 import SetAPIKey from "./SetAPIKey";
@@ -18,7 +21,7 @@ const App = () => {
 
   return (
     <ChakraProvider>
-      <Box p="8" fontSize="lg" w="full">
+      <Box p="8" pb="16" fontSize="lg" w="full">
         <HStack mb={4} alignItems="center">
           <Heading as="h1" size="lg" flex={1}>
             WebWand 🪄
@@ -38,6 +41,34 @@ const App = () => {
         ) : (
           <SetAPIKey />
         )}
+      </Box>
+      <Box
+        px="8"
+        pos="fixed"
+        w="100%"
+        bottom={0}
+        zIndex={2}
+        as="footer"
+        backdropFilter="auto"
+        backdropBlur="8px"
+      >
+        <HStack
+          columnGap="1.5rem"
+          rowGap="0.5rem"
+          fontSize="md"
+          borderTop="1px dashed gray"
+          py="3"
+          justify="center"
+          shouldWrapChildren
+          wrap="wrap"
+        >
+          <Link href="https://discord.gg/yfMjZ8udb5" isExternal>
+            Join Our Discord <Icon verticalAlign="text-bottom" as={FaDiscord} />
+          </Link>
+          <Link href="https://forms.gle/isLeGyUvoKGiqT8W8" isExternal>
+            Leave Feedbacks
+          </Link>
+        </HStack>
       </Box>
     </ChakraProvider>
   );
