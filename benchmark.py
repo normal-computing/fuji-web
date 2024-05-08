f7d685665205d311b3aa67e5b0116261a56185c4
Lingjie Feng <lynchee.owo@gmail.com>
2024-05-08 13:57:42 -0400

    feat: store action history to local files

--- benchmark.py
+++ benchmark.py
@@ -10,14 +10,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 import threading
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-import requests
 
 # Load the .env file
 load_dotenv()
 api_key = os.getenv('OPENAI_API_KEY')
 
 # Hard-coded coordinates to open web-wand side panel
-extensions_pos = (1050, 110)
+extensions_pos = (1060, 110)
 web_wand_pos = (900, 280)
 
 app = Flask(__name__)
@@ -25,8 +24,9 @@ CORS(app)
 
 task_status = "idle"
 task_history = []
+task_id = ""
 
-results_dir = 'result'
+results_dir = 'results'
 os.makedirs(results_dir, exist_ok=True)
 
 @app.route('/status', methods=['POST'])
@@ -38,11 +38,14 @@ def status():
 @app.route('/history', methods=['POST'])
 def history():
     global task_history
+    global task_id
     task_history = request.json.get('history', [])
 
-    file_name = f"{results_dir}/task_history_{int(time.time())}.txt"
-
-    with open(file_name, 'w') as file:
+    task_dir = os.path.join(results_dir, f"test{task_id}")
+    os.makedirs(task_dir, exist_ok=True)
+    file_path = os.path.join(task_dir, 'interact_messages.json')
+    
+    with open(file_path, 'w') as file:
         json.dump(task_history, file, indent=4)
     return jsonify({"message": "Got history"}), 200
 
@@ -67,31 +70,41 @@ def dispatch_event(driver, event_name, event):
 def check_task_status(driver):
     while task_status == "idle" or task_status == "running":
         dispatch_event(driver, 'GetTaskStatus', {})
-        time.sleep(5)
+        time.sleep(3)
     print(task_status)
 
-def run_webwand_task(driver, url, task_description):
-    driver.get(url)
-    # Simulate click to open side panel
-    pyautogui.click(extensions_pos)
-    pyautogui.click(web_wand_pos) 
-
+def run_webwand_task(driver, task_description):
     dispatch_event(driver, 'SetAPIKey', {"value": api_key})
     dispatch_event(driver, 'SetTask', {"value": task_description})
     dispatch_event(driver, 'RunTask', {})
 
     check_task_status(driver)
     dispatch_event(driver, 'GetTaskHistory', {})
-    
-    time.sleep(30)
+    time.sleep(1)
 
 def main():
     driver = setup_driver()
 
+    first_time = True
+
     with open('tasks_test.jsonl', 'r') as file:
+        global task_id
+        global task_status
+        global task_history
         for line in file:
             task = json.loads(line)
-            run_webwand_task(driver, task['web'], task['ques'])
+            task_id = task["id"]
+            task_status = "idle"
+            task_history = []
+            driver.get(task['web'])
+
+            if first_time:
+                # Simulate click to open side panel
+                pyautogui.click(extensions_pos)
+                pyautogui.click(web_wand_pos)
+                first_time = False
+
+            run_webwand_task(driver, task['ques'])
     driver.quit()
 
 if __name__ == "__main__":
